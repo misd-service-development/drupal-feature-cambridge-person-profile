67dae6bc56f680329c39953606d28a64f5c5831d
JeebsUK <my.friend.of.misery@gmail.com>
2013-12-10 10:35:55 +0000

    Added the view to display person profiles. Configured the horizontal teaser display mode to work with the teaser tpl and the view.

--- templates/node--cambridge-person-profile-teaser.tpl.php
+++ templates/node--cambridge-person-profile-teaser.tpl.php
@@ -1,46 +1,48 @@
 <?php
-$a = 1;
-hide($content['links']);
-
-if (array_key_exists('field_link', $content)):
-  hide($content['field_link']);
-  $url = $content['field_link']['#items'][0]['url'];
-  $read_more = $content['field_link']['#items'][0]['title'];
-
-  if (substr($read_more, 0, 80) == substr($url, 0, 80)):
-    $read_more = t('Read more');
-  endif;
-
-  if (array_key_exists('field_image', $content)):
-    $content['field_image'][0]['#path'] = array('path' => $url);
-  endif;
-else:
-  $url = $node_url;
-  $read_more = t('Read more');
-endif;
-
+$url = $node_url;
+$read_more = t('Read more');
 ?>
 <?php if ($teaser) : ?>
-<h2>Teaser Test</h2>
-<div class="campl-content-container <?php print $classes; ?>" <?php print $attributes; ?>>
-  <div class="campl-horizontal-teaser campl-teaser clearfix">
-    <div class="campl-column6">
-      <div class="campl-content-container campl-horizontal-teaser-img">
-        <?php print render($content['field_image']) ? : '&nbsp;'; ?>
-      </div>
+
+<div class="campl-content-container person-profile-teaser-item <?php print $classes; ?> " <?php print $attributes; ?>>
+
+    <div class="campl-horizontal-teaser campl-teaser clearfix">
+
+    <!-- use a two column 9/3 split for the top part of the person profile layout -->
+        <div class="campl-column9">
+            <div class="campl-content-container campl-horizontal-teaser-txt">
+                <?php print render($title_prefix); ?>
+                <h3><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
+                <?php print render($title_suffix); ?>
+
+                <?php if ($content['field_person_positions']) : ?>
+                    <?php print render($content['field_person_positions']); ?>
+                <?php endif; ?>
+
+                <?php if ($content['field_website']) : ?>
+                    <p>
+                        <!-- this field is always output as "personal website" - setting configured in content type display mode -->
+                        <?php print render($content['field_website']); ?>
+                    </p>
+                <?php endif; ?>
+                <?php if ($content['field_telephone_number']): ?>
+                    <?php print render($content['field_telephone_number']); ?>
+                <?php endif; ?>
+                <?php if ($content['field_email_address']): ?>
+                    <?php print render($content['field_email_address']); ?>
+                <?php endif; ?>
+
+                <div style="padding: 10px 0;">
+                <a href="<?php print $url; ?>" class="campl-primary-cta"><?php print $read_more; ?></a>
+                </div>
+            </div>
+        </div>
+        <div class="campl-column3">
+            <div class="campl-content-container campl-listing-img">
+                <?php print render($content['field_person_picture']); ?>
+            </div>
+        </div>
+
     </div>
-    <div class="campl-column6">
-      <div class="campl-content-container campl-horizontal-teaser-txt">
-        <?php print render($title_prefix); ?>
-        <h3 class='campl-teaser-title'><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
-        <?php print render($title_suffix); ?>
-        <?php if ($display_submitted): ?>
-          <p class="campl-datestamp"><?php print $date; ?></p>
-        <?php endif; ?>
-        <?php print render($content); ?>
-        <a href="<?php print $url; ?>" class="campl-primary-cta"><?php print $read_more; ?></a>
-      </div>
-    </div>
-  </div>
 </div>
 <?php endif; ?>
